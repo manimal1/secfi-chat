b8bb586b44c1898a1909714e1a18ee2bd24bedf4
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2020-07-15 13:42:59 +0200

    add notifications container

--- src/containers/RootContainer/RootContainer.tsx
+++ src/containers/RootContainer/RootContainer.tsx
@@ -2,8 +2,11 @@ import React, { FC } from "react";
 
 import { LandingRoute } from "+landing";
 
+import { NotificationsContainer } from "../NotificationsContainer";
+
 export const RootContainer: FC = () => (
   <main className="App">
     <LandingRoute />
+    <NotificationsContainer />
   </main>
 );
